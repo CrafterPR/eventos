3a618444c58f63c379eafdd052f58c0499056ec8
James Makau <jacjimus@gmail.com>
2024-09-06 16:46:06 +0300

    [EVT-6] adds creating checkpoints for event

---
+++ app/Http/Livewire/Events/Checkpoint.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Livewire\Events;
+
+use App\Enum\UserStatus;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Checkpoint as CheckpointModel;
+use Livewire\Component;
+
+class Checkpoint extends Component
+{
+    public CheckpointModel $checkpoint;
+
+    protected $rules = [
+        'checkpoint.name' => ['required', 'max:100', 'min:3', 'unique:checkpoints,name,event_id'],
+        'checkpoint.event_id' => ['required', 'exists:events,id'],
+        'checkpoint.is_gift_checkpoint' => ['sometimes', 'boolean'],
+        'checkpoint.leader_id' => ['required_if:is_gift_checkpoint,true', 'exists:users,id'],
+    ];
+
+    protected $messages = [
+        'checkpoint.leader_id' => 'You must assign an account to a gift checkpoint',
+    ];
+
+
+    public function mount($event_id)
+    {
+        $this->checkpoint = new CheckpointModel();
+        $this->checkpoint->event_id = $event_id;
+    }
+    public function render()
+    {
+        $users = User::query()
+            ->where('status', UserStatus::ACTIVE)
+            ->whereDoesntHave('roles', function ($query) {
+                $query->whereIn('name', [Role::SUPER_ADMIN]);
+            })
+            ->get();
+        return view('livewire.events.checkpoint', compact('users'));
+    }
+
+    public function submit()
+    {
+        $this->validate();
+        $this->checkpoint->save();
+        $this->dispatch('success', 'Checkpoint has been created successfully!');
+    }
+
+}
--- resources/views/pages/dashboards/index.blade.php
+++ resources/views/pages/dashboards/index.blade.php
@@ -8,10 +8,14 @@
 
     @can('view-quick-links')
         <div class="row g-5 g-xl-10 m-5 mb-xl-10">
-            <div class="row">
+            <div class="row card card-flush pb-6">
                 <!-- Dashboard Quick Links -->
+                <div class="card-header p-4">
+                    <h3 class="card-title align-items-start flex-column">
+                        <span class="card-label fw-bold text-gray-800">Quick Links</span>
+                    </h3>
+                </div>
                 <div class="col-12">
-                    <h4 class="mb-3">Quick Links</h4>
                     <div class="d-flex flex-wrap gap-2">
                         <!-- Transparent Link 1 -->
                         @can('checkin-event')
@@ -104,8 +108,66 @@
             <livewire:delegate.import-delegates-modal></livewire:delegate.import-delegates-modal>
     @endcan
     @can('view-dashboard')
-        <div class="row">
-            <div class="col-md-8 card card-flush h-md-100 mr-2">
+        <div class="row g-5 g-xl-10 mx-5 mp-n4 mb-xl-10">
+            <div class="d-flex align-items-center">
+                <h3 class="card-title align-items-start flex-column"></h3>
+                <div class="card-toolbar ms-auto"> <!-- Use 'ms-auto' to push it to the right -->
+                    <a href="#" class="btn btn-sm btn-light">
+                    <button type="button" class="btn btn-icon btn-active-light-primary w-30px h-30px ms-auto" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                        <i class="ki-duotone ki-setting-3 fs-3">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                            <span class="path3"></span>
+                            <span class="path4"></span>
+                            <span class="path5"></span>
+                        </i>
+                    </button>
+                        Filter by event</a>
+                    <!--begin::Task menu-->
+                    <div class="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true" data-kt-menu-id="kt-users-tasks">
+                        <!--begin::Header-->
+                        <div class="px-7 py-5">
+                            <div class="fs-5 text-dark fw-bold">Update Status</div>
+                        </div>
+                        <!--end::Header-->
+                        <!--begin::Menu separator-->
+                        <div class="separator border-gray-200"></div>
+                        <!--end::Menu separator-->
+                        <!--begin::Form-->
+                        <form class="form px-7 py-5" data-kt-menu-id="kt-users-tasks-form">
+                            <!--begin::Input group-->
+                            <div class="fv-row mb-10">
+                                <!--begin::Label-->
+                                <label class="form-label fs-6 fw-semibold">Status:</label>
+                                <!--end::Label-->
+                                <!--begin::Input-->
+                                <select class="form-select form-select-solid" name="task_status" data-kt-select2="true" data-placeholder="Select option" data-allow-clear="true" data-hide-search="true">
+                                    <option></option>
+                                    <option value="1">Approved</option>
+                                    <option value="2">Pending</option>
+                                    <option value="3">In Process</option>
+                                    <option value="4">Rejected</option>
+                                </select>
+                                <!--end::Input-->
+                            </div>
+                            <!--end::Input group-->
+                            <!--begin::Actions-->
+                            <div class="d-flex justify-content-end">
+                                <button type="button" class="btn btn-sm btn-light btn-active-light-primary me-2" data-kt-users-update-task-status="reset">Reset</button>
+                                <button type="submit" class="btn btn-sm btn-primary" data-kt-users-update-task-status="submit">
+                                    <span class="indicator-label">Apply</span>
+                                    <span class="indicator-progress">Please wait...
+                                                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                                </button>
+                            </div>
+                            <!--end::Actions-->
+                        </form>
+                        <!--end::Form-->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8 card card-flush h-md-100">
                 <div class="card-header pt-7 d-flex">
                     <h3 class="card-title align-items-start flex-column justify-content-center">
                         <span class="card-label fw-bold text-gray-800">Delegate Checkins by Date</span>
