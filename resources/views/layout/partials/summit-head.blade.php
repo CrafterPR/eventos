ec7029287092383bfcaf8b1e1aeca1b84a3ed505
James Makau <james@tiberbu.com>
2026-03-01 16:12:41 +0300

    Update summit-head.blade.php

--- resources/views/layout/partials/summit-head.blade.php
+++ resources/views/layout/partials/summit-head.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/country-select-js/2.1.1/css/countrySelect.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
     <script src="https://unpkg.com/alpinejs" defer></script>
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Include JustValidate -->
     <script src="https://cdn.jsdelivr.net/npm/just-validate@3/dist/just-validate.production.min.js"></script>
 
