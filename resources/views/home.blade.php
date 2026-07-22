ceada2e526359746c17add292a7a4b0ab280d075
James Makau <james@tiberbu.com>
2026-07-22 16:39:05 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <div class="relative h-screen w-full section-visible" id="home">
             <div class="absolute inset-0 bg-cover bg-center"
                  style="background-image:url('{{ asset('assets/media/images/landing.webp') }}');
-                 filter:brightness(0.9)"></div>
+                 filter:brightness(0.7)"></div>
             <div class="relative h-full flex items-center justify-center pt-4 sm:pt-2 md:pt-0">
                 <div class="mx-auto max-w-6xl w-full px-4 sm:px-6">
                     <div class="max-w-2xl mx-auto text-center">
