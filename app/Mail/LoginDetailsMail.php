9fa237395c3f3613ac623f8a2df48b8774ff197d
James Makau <james@tiberbu.com>
2026-09-01 11:28:52 +0300

    Update LoginDetailsMail.php

--- app/Mail/LoginDetailsMail.php
+++ app/Mail/LoginDetailsMail.php
@@ -14,7 +14,7 @@ class LoginDetailsMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
-    public User $user;
+    public  $user;
     public string $password;
     public ?PurchaseOrder $purchaseOrder = null;
 
