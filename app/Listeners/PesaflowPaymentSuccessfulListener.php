8445b01c6f9382054b97fc952c6cb1fc6a545aff
James Makau <james@tiberbu.com>
2026-09-01 10:38:19 +0300

    only create user after successful paymnet

--- app/Listeners/PesaflowPaymentSuccessfulListener.php
+++ app/Listeners/PesaflowPaymentSuccessfulListener.php
@@ -3,17 +3,14 @@
 namespace App\Listeners;
 
 use App\Events\PesaflowPaymentSuccessfulEvent;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\LoginDetailsMail;
 use App\Models\User;
 use App\Models\Role;
 
-class PesaflowPaymentSuccessfulListener implements ShouldQueue
+class PesaflowPaymentSuccessfulListener
 {
-    use InteractsWithQueue;
 
     /**
      * Handle the event.
