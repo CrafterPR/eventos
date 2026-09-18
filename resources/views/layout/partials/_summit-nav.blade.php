376337a41e61330655d313fd0fb8d95f58fdc72c
James Makau <james@tiberbu.com>
2026-09-18 10:27:53 +0300

    Update _summit-nav.blade.php

--- resources/views/layout/partials/_summit-nav.blade.php
+++ resources/views/layout/partials/_summit-nav.blade.php
@@ -49,7 +49,7 @@
 {{--                       class="text-[#172840] hover:text-gray-900 hover:bg-[#175C93] px-2 sm:px-3 py-1.5 rounded-full transition-all duration-300 cursor-pointer text-xs sm:text-sm">--}}
 {{--                        Tracks--}}
 {{--                    </a>--}}
-                    <a href="{{ asset('assets/media/images/2ND KICP SPONSORSHIP KIT.pdf') }}" target="_blank"
+                    <a href="{{ asset('assets/media/images/KICP SPONSORSHIP PROPOSAL& ORDER FORM.pdf') }}" target="_blank"
                        class="text-[#172840] hover:text-gray-900 hover:bg-[#175C93] px-2 sm:px-3 py-1.5 rounded-full
                        transition-all duration-300 cursor-pointer text-xs sm:text-sm">
                         Sponsorship
